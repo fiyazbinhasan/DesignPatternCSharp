3ebc8a5fac0bf3a81de5edb439fdb7bc9bf18912
fiyazbinhasan <fiyazhasan@live.com>
2018-09-18 18:23:32 +0600

    partial

---
+++ Prototype/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer cust = new Customer();
+            cust.FirstName = "Bla";
+            cust.LastName = "Gla";
+            cust.Address = new Address()
+            {
+                PrimaryAddress = "123",
+                SecondaryAddress = "232"
+            };
+
+            var address = (Address) cust.Address.Clone();
+            address.PrimaryAddress = "rent";
+
+            var customerClone = (Customer) cust.Clone();
+
+            Console.WriteLine(cust.Address.PrimaryAddress = "srw");
+
+            Console.WriteLine(address.PrimaryAddress);
+            Console.WriteLine(customerClone.Address.PrimaryAddress);
+
+            Console.ReadLine();
+        }
+    }
+}
